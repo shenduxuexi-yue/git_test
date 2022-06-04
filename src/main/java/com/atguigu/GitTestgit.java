79211884b80115ec775ba33cb94d0c9eabb2d440
ycy <0801@qq.com1398228998>
2022-06-04 15:52:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTestgit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author: ycy
+ * @Created: 2022/06/04/15:46
+ */
+public class GitTestgit {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
