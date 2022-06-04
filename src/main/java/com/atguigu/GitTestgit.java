8f99626c49cfcbed52f1d7eb4865133ca10c39e2
ycy <0801@qq.com1398228998>
2022-06-04 15:55:13 +0800

    second commit

--- src/main/java/com/atguigu/GitTestgit.java
+++ src/main/java/com/atguigu/GitTestgit.java
@@ -6,6 +6,7 @@ package com.atguigu;
  */
 public class GitTestgit {
     public static void main(String[] args) {
-        System.out.println("helloGit");
+        System.out.println("helloGit1");
+        System.out.println("helloGit2");
     }
 }
